bc6b6f3a79117f356fd3a56205e5fc609b8edeb5
brian d foy <brian.d.foy@gmail.com>
2015-01-03 17:05:12 -0500

    Update copyright statement

--- lib/Mac/PropertyList.pm
+++ lib/Mac/PropertyList.pm
@@ -840,7 +840,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2004-2014 brian d foy.  All rights reserved.
+Copyright © 2004-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
