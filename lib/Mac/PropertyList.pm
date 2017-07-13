5d802020a591fde0abd0bdcf349a0bc3f3865f01
Bram Stappers <bram.stappers@tiobe.com>
2017-07-13 16:56:57 +0200

    122466: [Mac-PropertyList] Fixed parsing .plist files with XML comments.
    Restored removal of all XML comments (<!-- -->) from the text.
    
    See ticket https://rt.cpan.org/Public/Bug/Display.html?id=122466 for
    details.

--- lib/Mac/PropertyList.pm
+++ lib/Mac/PropertyList.pm
@@ -194,6 +194,7 @@ sub parse_plist {
 
 	my $plist = do {
 		if( $text =~ /\A<\?xml/ ) { # XML plists
+			$text =~ s/<!--(?:[\d\D]*?)-->//g;
 			# we can handle either 0.9 or 1.0
 			$text =~ s|^<\?xml.*?>\s*<!DOC.*>\s*<plist.*?>\s*||;
 			$text =~ s|\s*</plist>\s*$||;
