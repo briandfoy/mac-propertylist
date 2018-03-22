a2c30acce94bb7cda94f604f198648a8b8e1704c
brian d foy <brian.d.foy@gmail.com>
2018-03-22 14:46:04 -0400

    Change to Artistic License 2.0

--- lib/Mac/PropertyList/ReadBinary.pm
+++ lib/Mac/PropertyList/ReadBinary.pm
@@ -395,7 +395,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 Copyright © 2004-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+it under the terms of the Artistic License 2.0.
 
 =cut
 
