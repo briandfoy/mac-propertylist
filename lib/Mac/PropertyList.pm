b4781df2294fe6d6152e11244bc19868d9d09575
brian d foy <brian.d.foy@gmail.com>
2018-03-22 14:46:15 -0400

    Update Copyright

--- lib/Mac/PropertyList.pm
+++ lib/Mac/PropertyList.pm
@@ -869,7 +869,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2004-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2004-2018, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the Artistic License 2.0.
